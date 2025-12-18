21bb9963997b4c7c780b3024160754c2e0fc16ea
Unbistrackted <112902220+Unbistrackted@users.noreply.github.com>
2025-12-18 12:19:27 -0300

    Add support for nekobako-plugin-utils dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,9 @@
+use nekobako_plugin_utils::{get_or_generate_config, is_enabled};
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 use skyline::hooks::{getRegionAddress, Region};
 use skyline::install_hook;
-use skyline_config::{SdCardStorage, StorageHolder};
-
-const BUILD_INFO: [u8; 20] = [
-    0x76, 0x16, 0xf8, 0x96, 0x3d, 0xac, 0xcd, 0x70, 0xe2, 0x0f, 0xf3, 0x90, 0x4e, 0x13, 0x36, 0x7f,
-    0x96, 0xf2, 0xd9, 0xb3,
-];
+use smart_default::SmartDefault;
 
 const GBL_PERSIST_SAVE_DATA_OFFSET: usize = 0x229220;
 const GBL_CG_SAVE_DATA_OFFSET: usize = 0x229280;
@@ -18,10 +14,12 @@ const PERSIST_SET_OFFSET: usize = 0xd12d0;
 const UNLOCK_ALL_OFFSET: usize = 0xe3050;
 const UNLOCK_FROM_DATA_OFFSET: usize = 0xd1930;
 
-static CONFIG: Lazy<Config> = Lazy::new(get_config);
+static CONFIG: Lazy<Config> =
+    Lazy::new(|| get_or_generate_config::<Config>(env!("CARGO_PKG_NAME")));
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, SmartDefault)]
 struct Config {
+    #[default = true]
     is_enabled: bool,
 }
 
@@ -78,46 +76,7 @@ unsafe fn unlock_all_function(param_1: u64, param_2: *mut i32, param_3: *mut i32
 
 #[skyline::main(name = "NekobakoUnlockAllMod")]
 pub fn main() {
-    if unsafe { !has_same_build_info() } {
-        return;
+    if is_enabled!(*CONFIG) {
+        install_hook!(unlock_all_function);
     }
-
-    if !CONFIG.is_enabled {
-        return;
-    }
-
-    install_hook!(unlock_all_function);
-}
-
-unsafe fn has_same_build_info() -> bool {
-    let data_adress = getRegionAddress(Region::Data) as usize;
-    let scan = core::slice::from_raw_parts((data_adress - 0x1000) as *const u8, 0x1000);
-
-    let gnu_end_pos = match scan.windows(4).position(|w| w == b"GNU\x00") {
-        Some(pos) => pos + 4,
-        None => return false,
-    };
-
-    let build_info = &scan[gnu_end_pos..gnu_end_pos + 20]; // In the decompilation BUILD INFO had 20 bytes
-
-    build_info == BUILD_INFO.as_slice()
-}
-
-fn get_config() -> Config {
-    let sd_storage = SdCardStorage::new(
-        "atmosphere/contents/01006A300BA2C000/romfs/skyline/config/nekobako_unlock_all_mod",
-    );
-
-    let mut storage_holder = StorageHolder::new(sd_storage);
-
-    //Creates the config if it doesn't exist
-    if !storage_holder.get_flag("config.yaml") {
-        let default_config = Config { is_enabled: true };
-
-        storage_holder
-            .set_field_yaml("config.yaml", &default_config)
-            .unwrap();
-    }
-
-    storage_holder.get_field_yaml("config.yaml").unwrap()
 }
